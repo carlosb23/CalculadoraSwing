9353565d6d1d5157c9c7b68673c61404de7d4a4f
Carlos <carlos.bustos.t@gmail.com>
2023-10-18 21:39:33 +0200

    Initial commit

---
+++ src/main/java/org/example/VentanaCarculadora.java
@@ -0,0 +1,319 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.lang.String.*;
+
+public class VentanaCarculadora extends JFrame {
+
+
+    private JPanel Calculadora;
+    private JTextField Pantalla;
+    private JButton xButton;
+    private JButton ACButton;
+    private JButton ONButton;
+    private JButton a7Button;
+    private JButton a4Button;
+    private JButton a8Button;
+    private JButton a2Button;
+    private JButton button12;
+    private JButton a1Button;
+    private JButton button14;
+    private JButton a5Button;
+    private JButton button9;
+    private JButton button17;
+    private JButton a9Button;
+    private JButton a6Button;
+    private JButton a3Button;
+    private JButton Button0;
+    private JButton Punto;
+    private JButton igualButton;
+    private JButton Raiz;
+    private boolean calculadoraEncendida = false;
+    double a, b, result;
+    String op;
+
+
+    public VentanaCarculadora() {
+
+        this.setContentPane(Calculadora);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Pantalla.setText("0");
+        pack();
+
+
+
+
+        ACButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pantalla.setText("");
+            }
+        });
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(a7Button.getText());
+                } else {
+                    Pantalla.setText(pantallaText + a7Button.getText());
+                }
+            }
+        });
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(a8Button.getText());
+                } else {
+                    Pantalla.setText(pantallaText + a8Button.getText());
+                }
+            }
+        });
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(a9Button.getText());
+                } else {
+                    Pantalla.setText(pantallaText + a9Button.getText());
+                }
+            }
+        });
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(a4Button.getText());
+                } else {
+                    Pantalla.setText(pantallaText + a4Button.getText());
+                }
+            }
+        });
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(a5Button.getText());
+                } else {
+                    Pantalla.setText(pantallaText + a5Button.getText());
+                }
+            }
+        });
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(a6Button.getText());
+                } else {
+                    Pantalla.setText(pantallaText + a6Button.getText());
+                }
+            }
+        });
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(a1Button.getText());
+                } else {
+                    Pantalla.setText(pantallaText + a1Button.getText());
+                }
+            }
+        });
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(a2Button.getText());
+                } else {
+                    Pantalla.setText(pantallaText + a2Button.getText());
+                }
+            }
+        });
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(a3Button.getText());
+                } else {
+                    Pantalla.setText(pantallaText + a3Button.getText());
+                }
+            }
+        });
+        Button0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.equals("0")) {
+                    Pantalla.setText(Button0.getText());
+                } else {
+                    Pantalla.setText(pantallaText + Button0.getText());
+                }
+            }
+        });
+        Punto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!Pantalla.getText().contains(".")){
+                    Pantalla.setText(Pantalla.getText() + Punto.getText());
+                }
+            }
+        });
+        button12.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                a = Double.parseDouble(Pantalla.getText());
+                op = "+";
+                Pantalla.setText("");
+            }
+        });
+        button14.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pantallaText = Pantalla.getText();
+                if (pantallaText.isEmpty()) {
+                    Pantalla.setText("-");
+                } else {
+                    a = Double.parseDouble(pantallaText);
+                    op = "-";
+                    Pantalla.setText("");
+                }
+            }
+        });
+        xButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                a = Double.parseDouble(Pantalla.getText());
+                op = "*";
+                Pantalla.setText("");
+            }
+        });
+
+        button9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                a = Double.parseDouble(Pantalla.getText());
+                op = "/";
+                Pantalla.setText("");
+            }
+        });
+        button17.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String backspace = null;
+                if(Pantalla.getText().length()>0){
+                    StringBuilder str = new StringBuilder(Pantalla.getText());
+                    str.deleteCharAt(Pantalla.getText().length() - 1);
+                    backspace = valueOf(str);
+                    Pantalla.setText(backspace);
+                }
+            }
+        });
+        igualButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                b = Double.parseDouble(Pantalla.getText());
+                if(op == "+"){
+                    result= a+b;
+                    Pantalla.setText(String.valueOf(result));
+                } else if (op == "-") {
+                    result= a-b;
+                    Pantalla.setText(String.valueOf(result));
+                }else if (op == "*") {
+                    result= a*b;
+                    Pantalla.setText(String.valueOf(result));
+                }else if (op == "/") {
+                    result= a/b;
+                    Pantalla.setText(String.valueOf(result));
+                }
+
+                if (result % 1 == 0) {
+                    Pantalla.setText(String.valueOf((int)result)); // Convierte result a int
+                } else {
+                    Pantalla.setText(String.valueOf(result)); // Muestra el número de coma flotante
+                }
+            }
+        });
+
+        //esto es una mejora que implemento apagarla y endenderla
+        ONButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton[] botones = {xButton,ACButton,a7Button,a4Button,a8Button,a2Button,button12,a1Button,button14,
+                        a5Button,button9,button17,a9Button,a6Button,a3Button,Button0,Punto,igualButton};
+                calculadoraEncendida = !calculadoraEncendida; // Cambia el estado de la calculadora
+                if (calculadoraEncendida) {
+                    ONButton.setText("ON");
+                    ONButton.setBackground(Color.GREEN);
+                    habilitarBotones(botones);
+                    Pantalla.setEnabled(true);
+                } else {
+                    ONButton.setText("OFF");
+                    ONButton.setBackground(Color.RED);
+                    deshabilitarBotones(botones);
+                }
+
+
+            }
+
+            private void deshabilitarBotones(JButton[] botones) {
+                for (JButton boton: botones){
+                    boton.setEnabled(false);
+                }
+                Pantalla.setEnabled(false);
+            }
+
+            private void habilitarBotones(JButton[] botones) {
+                for (JButton boton : botones){
+                    boton.setEnabled(true);
+                }
+            }
+        });
+        Raiz.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double numero = Double.parseDouble(Pantalla.getText());
+                if (numero >= 0) {
+                    //No sabia como calcular la raiz busque en internet y decian que se hace con sqrt y si que va
+                    double resultado = Math.sqrt(numero);
+                    Pantalla.setText(String.valueOf(resultado));
+                } else {
+                    Pantalla.setForeground(Color.red);
+                    Pantalla.setText("Error: No negativos");
+
+                    Timer timer = new Timer(4000, new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            Pantalla.setForeground(Color.BLACK);
+                            Pantalla.setText("");
+                        }
+                    });
+                    timer.setRepeats(false);
+                    timer.start();
+
+                }
+            }
+        });
+    }
+
+    public void load(){
+        setVisible(true);
+    }
+}
